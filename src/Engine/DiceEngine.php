99e6b5c4069b2fb6412fb26158f1d8d9ae69302a
RafalMalik <rafalmalik.info@gmail.com>
2020-04-05 17:34:05 +0200

    Created empty DiceEngine class.

---
+++ src/Engine/DiceEngine.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * LoteriomaDiceEngine - application enabling the operation of gambling cubes.
+ *
+ * The application consists of a dice game engine based on a pseudo-randomity for which
+ * the external RNG component is used. The application is only responsible for handling
+ * the game logic and forwards its results to the Core component. Data used in the operation
+ * of the application are downloaded from the DataStore component.
+ *
+ * See more: https://raspberryvision.github.io/loterioma-dice-engine/.
+ *
+ * DiceEngine - casino dice game server.
+ * @see https://github.com/RaspberryVision/loterioma-dice-engine
+ *
+ * This code is part of the LoterioMa casino system.
+ * @see https://github.com/RaspberryVision/loterioma
+ *
+ * Created by Rafal Malik.
+ * 15:47 02.04.2020, Warsaw/Zabki - DELL
+ */
+
+namespace App\Engine;
+
+/**
+ * Implementation of the cube game engine using a random number generator.
+ * @category   Engine
+ * @package    App\Engine
+ * @author     Rafal Malik <rafalmalik.info@gmail.com>
+ * @copyright  03.2020 Raspberry Vision
+ */
+class DiceEngine
+{
+
+}
