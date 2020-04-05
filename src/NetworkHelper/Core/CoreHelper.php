b16dbc78690864b44b9f3706e56c301e0239384e
RafalMalik <rafalmalik.info@gmail.com>
2020-04-05 21:43:28 +0200

    Created CoreHelper.php.

---
+++ src/NetworkHelper/Core/CoreHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\NetworkHelper\Core;
+
+use App\Model\Network\NetworkRequest;
+use App\Model\Network\NetworkRequestInterface;
+use App\Model\Network\NetworkResponseInterface;
+use App\NetworkHelper\AbstractNetworkHelper;
+
+/**
+ * Curl helper to core requests.
+ * Helper enabling communication with the casino nucleus, it performs all operations
+ * on the network after the end of the game.
+ * @category   NetworkHelper
+ * @package    App\NetworkHelper\Core
+ * @author     Rafal Malik <rafalmalik.info@gmail.com>
+ * @copyright  03.2020 Raspberry Vision
+ */
+class CoreHelper extends AbstractNetworkHelper
+{
+    /**
+     * RNGHelper constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'loterioma_core_helper',
+            'http://loterioma_core',
+            80
+        );
+    }
+
+    /**
+     * Method returns
+     * @param Round $round
+     * @return NetworkResponseInterface
+     */
+    public function processRound(Round $round): NetworkResponseInterface
+    {
+        return $this->makeRequest(new NetworkRequest(
+            '/index.php/game/',
+            'POST',
+            'asdasdsad',
+            $round
+        ));
+    }
+}
