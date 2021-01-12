43f5b49f9af3755e90dbfba68f9ba80884394ee7
RafalMalik <rafalmalik.info@gmail.com>
2021-01-12 20:24:23 +0100

    Added Bet entity and repository.

---
+++ src/Entity/Bet.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BetRepository::class)
+ */
+class Bet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $rate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Round::class, inversedBy="bets")
+     */
+    private $round;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getRound(): ?Round
+    {
+        return $this->round;
+    }
+
+    public function setRound(?Round $round): self
+    {
+        $this->round = $round;
+
+        return $this;
+    }
+}
